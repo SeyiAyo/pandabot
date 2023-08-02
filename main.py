06f1864b750c388dea7788452084b6c179792034
SeyiAyo <110205697+SeyiAyo@users.noreply.github.com>
2023-08-02 01:36:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def save_knowledge_base(file_path :str, data: dict):
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=2)
 
-def find_best_match(user_question: str, questions: list) -> str | None:
+def find_best_match(user_question: str, questions: list) -> Optional[str]:
     matches: list = get_close_matches(user_question, questions, n=1, cutoff=0.6)
     return matches[0] if matches else None
 
